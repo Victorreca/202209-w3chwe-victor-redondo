ca37d0cc6a688365947f20d22c7cdc711dbd5752
Victor Redondo <vrviktor@gmail.com>
2022-10-16 19:19:13 +0200

    Edit poke list and add style.css

--- src/ts/components/PokeList/PokeList.ts
+++ src/ts/components/PokeList/PokeList.ts
@@ -1,2 +1,12 @@
+import Component from "../Component/Component.js";
+import type { Pokemon } from "../../pokemonTypes.js";
 
-
+export class PokeList extends Component {
+  constructor(
+    parentElement: HTMLElement,
+    private readonly pokemons: Pokemon[]
+  ) {
+    super(parentElement, "poke-list", "div");
+  }
+}
+export default PokeList;
